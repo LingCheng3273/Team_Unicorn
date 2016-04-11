0fd2a66bf7c8d46576dd9baeb3578150976ba1de
Jordan Louie <Jordan Louie>
2016-04-11 11:13:53 -0400

    getResponse fix for I want statement

--- MagpieActivityStarterCode/activity4/Magpie4.java
+++ MagpieActivityStarterCode/activity4/Magpie4.java
@@ -49,6 +49,10 @@ public class Magpie4
 	    }
 
 	// Responses which require transformations
+	else if (findKeyword(statement, "I want", 0) >= 0)
+	    {
+		response = transformIWantStatement(statement);
+	    }
 	else if (findKeyword(statement, "I want to", 0) >= 0)
 	    {
 		response = transformIWantToStatement(statement);
